03b0405dc7f0aaa18c8b8bd13b382cecf8e7b1d1
Nazanin <nazanin.reyh@gmail.com>
2025-01-30 13:45:33 -0500

    Upgrade packages

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="p-5 text-lg text-center bg-green-50">
-    Welcome to Auctria's technical interview. Feel free to remove this component and start coding!
-  </div>
+  <Hero />
 </template>
+
+<script setup>
+import Hero from './Hero.vue'
+</script>
---
+++ src/Hero.vue
@@ -0,0 +1,7 @@
+<template>
+   <div class="flex items-center justify-center w-screen h-screen p-5 bg-green-50">
+    <h1 class="text-2xl">    
+      Welcome to Auctria's technical interview. Feel free to remove this component and start coding!
+    </h1>
+  </div>
+</template>
